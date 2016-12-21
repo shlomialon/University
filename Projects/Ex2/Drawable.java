5231e80ff12f74e88f47b03d0e3217f56c11a8b1
shlomialon <shlomi.al90@gmail.com>
2016-12-21 09:12:14 +0200

    add Ex2 in OOP

---
+++ Projects/Ex2/Drawable.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Shlomi Alon on 19/12/2016.
+ */
+public interface Drawable {
+    public boolean equals(Drawable d);
+    public boolean contains(Point p);
+    public double perimeter();
+    public double area();
+    public void translate(Point p);
+}
---
+++ Projects/Ex2/ShapeContainer.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Shlomi Alon on 19/12/2016.
+ */
+public class ShapeContainer {
+}
