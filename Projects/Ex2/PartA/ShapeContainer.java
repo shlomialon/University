c04e5e33aeb33259ec93e5c66bc528ec2722cdca
shlomialon <shlomi.al90@gmail.com>
2016-12-28 21:32:22 +0200

    fixed project part a

--- Projects/Ex2/PartA/ShapeContainer.java
+++ Projects/Ex2/PartA/ShapeContainer.java
@@ -6,7 +6,6 @@ public class ShapeContainer {
 
     public static final int INIT_SIZE = 10;
     public static final int RESIZE = 10;
-
     public Drawable[] drawables;
     private int size;
 
@@ -16,11 +15,8 @@ public class ShapeContainer {
     }
 
     public ShapeContainer(ShapeContainer other) {
-
-        if (other == null) return;
-
         drawables = new Drawable[other.drawables.length];
-
+        if (other == null) return;
         for (int i = 0; i < drawables.length; i++) {
             if (other.drawables[i] != null) {
                 if (other.drawables[i] instanceof Rectangle) {
@@ -29,8 +25,8 @@ public class ShapeContainer {
                     drawables[i] = new Triangle((Triangle) other.drawables[i]);
                 }
             }
+            size = drawables.length;
         }
-        size = drawables.length;
     }
 
     public int T_size() {
@@ -52,21 +48,21 @@ public class ShapeContainer {
     }
 
     public int size() {
-        return (T_size() + R_size());
+        return R_size() + T_size();
     }
 
     public void add(Drawable d) {
         if (d != null) {
             if (d instanceof Rectangle) {
                 if (size() >= drawables.length) {
-                    R_resize();
+                    resize();
                 }
-                drawables[size()] =  d;
+                drawables[size()] = d;
                 size++;
             }
             else{
                 if (size() >= drawables.length) {
-                    T_resize();
+                    resize();
                 }
                 drawables[size()] = d;
                 size++;
@@ -74,48 +70,54 @@ public class ShapeContainer {
         }
     }
 
-    private void R_resize() {
-        Drawable[] arr = new Drawable[drawables.length + RESIZE];
-        System.arraycopy(drawables, 0, arr, 0, R_size());
-        drawables = arr;
-    }
-
-    private void T_resize() {
-        Drawable[] arr = new Drawable[drawables.length + RESIZE];
-        System.arraycopy(drawables, 0, arr, 0, T_size());
-        drawables = arr;
+    private void resize() {
+        Drawable temp[] = new Drawable[this.size + RESIZE];
+        for (int i = 0; i < this.size; i++) {
+            temp[i] = this.drawables[i];
+        }
+        this.drawables = temp;
     }
 
     public void remove(Point p) {
         if (p != null) {
-
             for (int i = 0; i < size; i++) {
-                if (drawables[i] != null){
-                    if (drawables[i].contains(p)) {
-                            size--;
-                        }
+                if (drawables[i] != null && drawables[i].contains(p)){
                         for (int j = i; j < drawables.length-1; j++) {
                             drawables[j] = drawables[j+1];
                         }
-                            i--;
+                    this.drawables[size-1] = null;
+                    this.size--;
+                    i--;
                         }
                     }
                 }
             }
 
     public Triangle T_at(int p) {
+        int index = 0;
         if (p < size() && drawables[p] != null) {
-            if (drawables[p] instanceof Triangle) {
-              return (Triangle) drawables[p];
+            for (int i = 0; i < size; i++) {
+                if (drawables[i] instanceof Triangle) {
+                    if (index == p) {
+                        return new Triangle((Triangle) drawables[i]);
+                    }
+                    index++;
+                }
             }
         }
         return null;
     }
 
     public Rectangle R_at(int p) {
+        int index = 0;
         if (p < size() && drawables[p] != null) {
-            if (drawables[p] instanceof Rectangle) {
-               return ((Rectangle) drawables[p]);
+            for (int i = 0; i < size; i++) {
+                if (drawables[i] instanceof Rectangle) {
+                    if(index == p) {
+                        return new Rectangle((Rectangle) drawables[i]);
+                    }
+                    index++;
+                }
             }
         }
         return null;
@@ -124,8 +126,13 @@ public class ShapeContainer {
     public double sumArea() {
         double sum = 0;
         for (int i = 0; i < size(); i++) {
-            if(drawables[i] != null)
-            sum += drawables[i].area();
+            if(drawables[i] instanceof Rectangle && drawables[i] != null){
+                Drawable drawable = drawables[i];
+                sum += drawable.area();
+            }else if(drawables[i] instanceof Triangle && drawables[i] != null){
+                Drawable drawable = drawables[i];
+                sum += drawable.area();
+            }
         }
         return sum;
     }
@@ -138,6 +145,5 @@ public class ShapeContainer {
             }
         }
     }
-
 }
 
